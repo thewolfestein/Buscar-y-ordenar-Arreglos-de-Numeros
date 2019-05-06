28aad5a412a3ab7d52471c236cd0c3c688b60815
thewolfestein <49486412+thewolfestein@users.noreply.github.com>
2019-05-06 19:37:48 -0400

    Update busquedas.h

--- busquedas.h
+++ busquedas.h
@@ -33,22 +33,25 @@ int BuscarEnOrden(ListaNumeros *UnaListaNumeros,int numero){
 
 int InsertarEnOrden(ListaNumeros *UnaListaNumeros,int nuevo_tamano){
 	
-	int i,j,x,aux;
+	int i,j,x,aux,dato;
 	int MAX=50000;
 	for(x=0;x<=nuevo_tamano;x++){
-	  	UnaListaNumeros->arreglo[x]=rand()%(MAX+1);	
+	  	UnaListaNumeros->arreglo[x]=dato;	
+	  	dato=dato+1;
 	  	UnaListaNumeros->cantidad_actual++;
 	}
 	//Ordenar lista ascendentemente (Necesario para utilizar la búsqueda binaria)
-	for(i=0;i<=nuevo_tamano;i++){	
+/*	for(i=0;i<=nuevo_tamano;i++){	
 		for(j=i+1;j<nuevo_tamano;j++){
 			if(UnaListaNumeros->arreglo[j]<UnaListaNumeros->arreglo[i]){
 				aux=UnaListaNumeros->arreglo[j];
 				UnaListaNumeros->arreglo[j]=UnaListaNumeros->arreglo[i];
 				UnaListaNumeros->arreglo[i]=aux;
+			
 			}
 		} 	
-	}
+		//	printf("\n %i",UnaListaNumeros->arreglo[i]);
+	}*/
 
 	UnaListaNumeros->cantidad_actual=UnaListaNumeros->cantidad_maxima;
 	
@@ -58,28 +61,30 @@ int InsertarEnOrden(ListaNumeros *UnaListaNumeros,int nuevo_tamano){
 
 int BusquedaBinaria(ListaNumeros *UnaListaNumeros, int nuevo_tamano,int numero){
 	
-	int indice,minimo,maximo;
+	int minimo,maximo,mitad;
 	minimo=0;
-	maximo=nuevo_tamano;
-	indice=((minimo+maximo)/2);
+	maximo=nuevo_tamano-1;
 	
-	while(maximo>minimo){	
-		if(UnaListaNumeros->arreglo[indice]<numero){
-			minimo=indice+1;
-			indice=(((minimo)+maximo)/2);	
+	
+	while(minimo <= maximo){	
+		mitad=((minimo+maximo)/2);
+		if(UnaListaNumeros->arreglo[mitad]==numero){
+			printf("ENCONTRAR %i",numero);
+			return numero;
+			
 		}else{
-				if(UnaListaNumeros->arreglo[indice]>numero){	
-					maximo=indice-1;
-					indice=((minimo+maximo)/2);
+				if(numero<UnaListaNumeros->arreglo[mitad]){	
+					maximo=mitad-1;
+					
 				}else{
-						if(UnaListaNumeros->arreglo[indice]==numero){
-							return numero;
+						minimo=mitad+1;
 						}
 				}	
-		}
-	}
-	return 0;
+	}	
+//	printf("hola%i",numero);
+	return 0;	
 }
+	
 
 // (3) Insertar siempre al final, ordenar con 'quicksort' después de cada inserción y usar búsqueda binaria:
 
@@ -104,13 +109,14 @@ int InsertarAlFinalQuickSort(ListaNumeros *UnaListaNumeros,int nuevo_tamano){
 	int MAX=50000;
 	for(i=nuevo_tamano;i>=0; i--){ 
 		UnaListaNumeros->arreglo[i]=rand()%(MAX+1);	
-		qsort(UnaListaNumeros->arreglo,nuevo_tamano, sizeof(int),Comparar);
 		UnaListaNumeros->cantidad_actual++;
+		//printf("\n %i",UnaListaNumeros->arreglo[i]);
 		
 	}
 	
+	qsort(UnaListaNumeros->arreglo,nuevo_tamano, sizeof(int),&Comparar);
+		
 	UnaListaNumeros->cantidad_actual=UnaListaNumeros->cantidad_maxima;
 	
 	return 1;
-	
 }
