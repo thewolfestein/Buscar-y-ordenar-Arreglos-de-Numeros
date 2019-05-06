4671ef91f19374c016019ab14452bbd37a3ea853
thewolfestein <49486412+thewolfestein@users.noreply.github.com>
2019-05-06 19:39:24 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,19 +5,18 @@
 
 int main() {
 	
-	int tamano=20000;
-	int numero=30648;
+	int tamano=100000000;
+	int numero=30000;
 	
 	ListaNumeros *UnaListaNumeros;
-	printf("COMIENZO DEL EXPERIMENTO 1");
+	printf("COMIENZO DEL EXPERIMENTO 1 \n");
 	UnaListaNumeros = CrearListaNumerosVacia(tamano);
 	Experimento(UnaListaNumeros,tamano,numero); 
-	printf("COMIENZO DEL EXPERIMENTO 2"); 
+	printf("COMIENZO DEL EXPERIMENTO 2 \n"); 
 	UnaListaNumeros = CrearListaNumerosVacia(tamano);
 	ExperimentoDos(UnaListaNumeros,tamano,numero);
-	printf("COMIENZO DEL EXPERIMENTO 3"); 
+	printf("COMIENZO DEL EXPERIMENTO 3 \n"); 
 	UnaListaNumeros = CrearListaNumerosVacia(tamano);
-	ExperimentoTres(UnaListaNumeros, tamano,numero);
+	ExperimentoTres(UnaListaNumeros,tamano,numero);
 
 }
-
